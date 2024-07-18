cd2e076b887ba865da68785263ba5548d34d5e32
mayrabevacqua <mayrabevacqua@gmail.com>
2024-07-18 19:40:48 -0300

    Agregado de producto y js en relación

---
+++ script.js
@@ -0,0 +1,17 @@
+let arrProductos=["Tirabuzón", "Producto x"]
+let arrPrecio=   [1400,         800]
+let arrCant=      [100,         50]
+
+document.querySelector("div button").addEventListener("click", ()=>{
+    let cantidad=document.querySelector("div input").value;
+    cantidad=Number(cantidad);
+    if(cantidad>0&&(arrCant[0]-cantidad)>0){
+        comprar(cantidad);
+    }else{
+        alert("ingrese una cantidad mayor a cero o no hay stock")
+    }
+})
+
+function comprar(cantidad) {
+    document.getElementById("total").innerText=cantidad*arrPrecio[0];
+}
