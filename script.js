e562c5678a6734790d0ae6ee26066ce3b3f4abdc
Alejandro Heinrich <ale.heinrich@gmail.com>
2024-07-19 12:55:26 -0300

    pruebas en js

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 /* ---------- JS DE COMPRAS ---------- */
 
-let arrProductos=["Tirabuzón", "Producto x"]
-let arrPrecio=   [1400,         800]
-let arrCant=      [10,         50]
+/*let arrProductos=["Tirabuzón", "Aceite de girasol", "Aceite de Oliva"];*/
 
-document.querySelector("div button").addEventListener("click", ()=>{
+
+
+
+
+
+
+let arrPrecios= document.querySelectorAll("#ale");
+
+
+
+
+
+
+
+/*let arrValPrecios = [];
+const arrCant = document.querySelectorAll("div input");
+let arrBotones = document.querySelectorAll("div button");
+
+/*for (i=0; i< arrPrecios.length; i++) {
+    arrValPrecios.push(arrPrecios[i]);
+}
+
+/*for(i=0 ; i < arrBotones.length ; i++){
+    arrBotones[i].addEventListener("click", ()=>{
+        let cantidad=document.querySelector("div input").value;
+        cantidad=Number(cantidad);
+        if(cantidad>0&&(arrCant[0]-cantidad)>0){
+            comprar(cantidad);
+        }else{
+            alert("ingrese una cantidad mayor a cero o no hay stock")
+        }
+    })
+    }*/
+/*document.querySelector("div button").addEventListener("click", ()=>{
     let cantidad=document.querySelector("div input").value;
     cantidad=Number(cantidad);
     if(cantidad>0&&(arrCant[0]-cantidad)>0){
@@ -13,7 +44,24 @@ document.querySelector("div button").addEventListener("click", ()=>{
         alert("ingrese una cantidad mayor a cero o no hay stock")
     }
 })
+/*__________PROBADOR DE ARRAYS__________*/
+document.querySelector(".probarArray").addEventListener("click", ()=> {
+        /*for (i=0; i<arrPrecios.length; i++) {
+    
+        }*/
+        alert(arrPrecios.length);
+})
+/*for(i=0; i< arrBotones.length; i++) {
+    let c=i;;
+    arrBotones[i].addEventListener("click", ()=>{
+        let palabraMostar;
+            let palabra=arrProductos[c];
+            palabraMostar=palabra;
+        alert("es el boton del prodcuto: ");
+        alert(palabraMostar);
 
+    })
+}
 function comprar(cantidad) {
     document.getElementById("total").innerText=cantidad*arrPrecio[0];
 }
