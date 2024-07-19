c5042fa37ffd4061dcbde45029f2fd979d559c10
Alejandro Heinrich <ale.heinrich@gmail.com>
2024-07-19 13:52:07 -0300

    carga de arrays

--- script.js
+++ script.js
@@ -1,27 +1,33 @@
 /* ---------- JS DE COMPRAS ---------- */
 
-/*let arrProductos=["Tirabuzón", "Aceite de girasol", "Aceite de Oliva"];*/
+arrProductos=["Tirabuzón", "Aceite de girasol", "Aceite de Oliva"];
 
 
 
 
 
 
-
-let arrPrecios= document.querySelectorAll("#ale");
-
-
-
-
-
-
-
-/*let arrValPrecios = [];
-const arrCant = document.querySelectorAll("div input");
+let arrValPrecios = [];
+let arrCant = document.querySelectorAll("div input");
 let arrBotones = document.querySelectorAll("div button");
+let arrSpanPrecios= document.querySelectorAll("#ale");
+let arrPrecios= [];
+let arrCantidad = []
 
-/*for (i=0; i< arrPrecios.length; i++) {
-    arrValPrecios.push(arrPrecios[i]);
+for (i=0; i< arrSpanPrecios.length; i++) {
+    arrPrecios[i]= parseInt (arrSpanPrecios[i].textContent);
+}
+for (i=0; i< arrCant.length ; i++){
+    arrCantidad[i] = arrCant[i].value;
+}
+/*for(i=0; i< arrBotones.length; i++) {
+    let c=i;;
+    arrBotones[i].addEventListener("click", ()=>{
+        alert("el precio del producto es: "+arrPrecios[c])
+    })
+}
+function agregar(cantidad) {
+    document.getElementById("total").innerText=cantidad*arrPrecio[0];
 }
 
 /*for(i=0 ; i < arrBotones.length ; i++){
@@ -49,22 +55,10 @@ document.querySelector(".probarArray").addEventListener("click", ()=> {
         /*for (i=0; i<arrPrecios.length; i++) {
     
         }*/
-        alert(arrPrecios.length);
+        alert(arrCantidad.length);
+        alert("La cantidad del producto 1 seleccionada es: "+ arrCantidad[0]);
 })
-/*for(i=0; i< arrBotones.length; i++) {
-    let c=i;;
-    arrBotones[i].addEventListener("click", ()=>{
-        let palabraMostar;
-            let palabra=arrProductos[c];
-            palabraMostar=palabra;
-        alert("es el boton del prodcuto: ");
-        alert(palabraMostar);
-
-    })
-}
-function comprar(cantidad) {
-    document.getElementById("total").innerText=cantidad*arrPrecio[0];
-}
+/*
 
 /* ---------- JS DE CONTACTO ---------- */
 
