d6f4d8cc33d42429d2e273f967fb86059e51dfec
mayrabevacqua <mayrabevacqua@gmail.com>
2024-07-22 16:42:13 -0300

    Agregado de Js correspondiente al formulario

--- script.js
+++ script.js
@@ -19,6 +19,38 @@ function comprar(cantidad) {
 }
 
 /* ---------- JS DE CONTACTO ---------- */
+document.addEventListener("DOMContentLoaded", () => {
+    
+    let nombre = document.querySelector('input[type="text"][placeholder="Nombre"]');
+    let apellido = document.querySelector('input[type="text"][placeholder="Apellido"]');
+    let email = document.querySelector('input[type="email"][placeholder="Email"]');
+    let codArea = document.querySelector('input[type="tel"][placeholder="Cod. Area (sin 0)"]');
+    let celular = document.querySelector('input[type="tel"][placeholder="Cel (sin 15)"]');
+    let btnEnviar = document.querySelector('button');
+
+    btnEnviar.addEventListener("click", (e) => {
+        e.preventDefault(); 
+
+        let informacion = [
+            nombre.value.trim(), 
+            apellido.value.trim(), 
+            email.value.trim(), 
+            codArea.value.trim(), 
+            celular.value.trim() 
+        ];
+
+        console.log(`Información del formulario:`);
+        console.log(`Nombre: ${informacion[0]}`);
+        console.log(`Apellido: ${informacion[1]}`);
+        console.log(`Email: ${informacion[2]}`);
+        console.log(`Cod. Area: ${informacion[3]}`);
+        console.log(`Celular: ${informacion[4]}`);
+
+        let blob = new Blob([informacion.join("\n")], { type: "text/plain;charset=utf-8" });
+
+        saveAs(blob, "contacto.txt");
+    });
+});
 
 
 /* ---------- JS DE INDEX ---------- */
