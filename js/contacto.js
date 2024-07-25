814972d02a058ef111e6a275a791cf1f33a3b9e5
mayrabevacqua <mayrabevacqua@gmail.com>
2024-07-25 15:48:19 -0300

    Separación de css y js, cambios en diseño

--- js/contacto.js
+++ js/contacto.js
@@ -1,24 +1,3 @@
-/* ---------- JS DE COMPRAS ---------- */
-
-let arrProductos=["Tirabuzón", "Producto x"]
-let arrPrecio=   [1400,         800]
-let arrCant=      [10,         50]
-
-document.querySelector("div button").addEventListener("click", ()=>{
-    let cantidad=document.querySelector("div input").value;
-    cantidad=Number(cantidad);
-    if(cantidad>0&&(arrCant[0]-cantidad)>0){
-        comprar(cantidad);
-    }else{
-        alert("ingrese una cantidad mayor a cero o no hay stock")
-    }
-})
-
-function comprar(cantidad) {
-    document.getElementById("total").innerText=cantidad*arrPrecio[0];
-}
-
-/* ---------- JS DE CONTACTO ---------- */
 document.addEventListener("DOMContentLoaded", () => {
     
     let nombre = document.querySelector('input[type="text"][placeholder="Nombre"]');
@@ -51,6 +30,3 @@ document.addEventListener("DOMContentLoaded", () => {
         saveAs(blob, "contacto.txt");
     });
 });
-
-
-/* ---------- JS DE INDEX ---------- */
