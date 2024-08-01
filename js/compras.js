495a94f385a7e13bd58bbbfba6b6d2588cdec053
mayrabevacqua <mayrabevacqua@gmail.com>
2024-07-31 21:29:56 -0300

    pagina productos funcionando

--- js/compras.js
+++ js/compras.js
@@ -6,33 +6,40 @@ const productos=["Fideos Tirabuzon N28 Matarazzo 500 Gr.", "Aceite de girasol Na
     "Gaseosa Coca Cola sabor original 2,25L.", "Harina de trigo 000 Favorita 1Kg."];
 const stock = [10,5,5,5,5,4,5,5,5,5,5,5];
 const precios = [1400,2600,9830,860,1259,3870,1825,3229,3840,1495,3100,980];
-
+let total = 0; 
 
 function pintarProductos(arrayProductos, arrayPrecios, arrayStock) {
     for (let i=0; i<arrayProductos.length; i++) {
         lista.innerHTML += `<li class="tamaño">
         <img class="foto" src="./css/imágenes/${i}.png" alt="producto"/>
         <p>${arrayProductos[i]}</p> 
-        <p class="precio">Precio: $ ${arrayPrecios[i]}</p>
-        <p>Stock: ${arrayStock[i]}</p>
-        <input type="hidden" id="stock ${i}" value="${arrayStock[i]}"></input>
+        <p class="precio">Precio: $${arrayPrecios[i]}</p>
+        <p>Stock: <span id="stock${i}">${arrayStock[i]}</span></p>
         <input class="entrada" type="number" id="entrada${i}" placeholder="Cant."></input>
         <button id="btn${i}">Agregar</button>
         </li>`
+    }
+    agregarEventos(); 
+}
+
+function agregarEventos() {
+    for (let i = 0; i < productos.length; i++) {
+        document.getElementById(`btn${i}`).addEventListener('click', () => comprar(i));
     }
 }
 
 function comprar (index) {
     const stockHTML = document.getElementById(`stock${index}`);
     const entradaHTML = document.getElementById(`entrada${index}`);
-    const stock = parseInt(stockHTML.value);
+    let stockDisponible = parseInt(stockHTML.innerText);
     const cantidad = parseInt(entradaHTML.value);
     const precio = precios[index];
 
-    if (cantidad > 0 && cantidad <= stock) {
-        total+= cantidad*precio
-        stockHTML.value = stock - cantidad;
+    if (cantidad > 0 && cantidad <= stockDisponible) {
+        total+= cantidad*precio;
+        stockHTML.innerText = stockDisponible - cantidad;
         entradaHTML.value = "";
+        document.getElementById("total").innerText = total;
         alert("Producto agregado");
     } else{
         alert("Cantidad no válida, revise que esté dentro de las cantidades disponibles de stock")
